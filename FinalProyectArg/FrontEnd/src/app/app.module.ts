cbaeba4ae229d242e135e1bd70d33fdc2219db29
enzxD <enzo_15enano@hotmail.com>
2023-03-22 13:08:50 -0300

    tratando de subir toda la base

--- FinalProyectArg/FrontEnd/src/app/app.module.ts
+++ FinalProyectArg/FrontEnd/src/app/app.module.ts
@@ -22,6 +22,9 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia/edit-experiencia.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { NewSkillComponent } from './components/hys/new-skill.component';
+import { EditSkillComponent } from './components/hys/edit-skill.component';
+import { EditAcercaDeComponent } from './components/acerca-de/edit-acerca-de.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -40,6 +43,9 @@ import { EditeducacionComponent } from './components/educacion/editeducacion.com
     EditExperienciaComponent,
     NeweducacionComponent,
     EditeducacionComponent,
+    NewSkillComponent,
+    EditSkillComponent,
+    EditAcercaDeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ FinalProyectArg/FrontEnd/src/app/model/skill.ts
@@ -0,0 +1,13 @@
+export class Skill {
+    id: number;
+    nombre: string;
+    porcentaje: number;
+
+    constructor(nombre:string, porcentaje:number){
+        this.nombre = nombre;
+        this.porcentaje = porcentaje;
+    }
+
+
+
+}
