003ad0e3e8ca7a98038c69ce47507aa24f23cfb7
enzxD <enzo_15enano@hotmail.com>
2023-03-22 12:03:30 -0300

    actualizacion deploy koyeb

--- FinalProyectArg/BackEnd/src/main/java/com/portfolio/EFS/Repository/IPersonaRepository.java
+++ FinalProyectArg/BackEnd/src/main/java/com/portfolio/EFS/Repository/IPersonaRepository.java
@@ -6,10 +6,12 @@
 package com.portfolio.EFS.Repository;
 
 import com.portfolio.EFS.Entity.Persona;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IPersonaRepository extends JpaRepository<Persona,Long>{
-    
+public interface IPersonaRepository extends JpaRepository<Persona,Integer>{
+    public Optional <Persona> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
 }
